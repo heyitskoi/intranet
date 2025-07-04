c9e0219d1fa4d9dfb3a70653d19d6ed239c4c41e
heyitskoi <112921161+heyitskoi@users.noreply.github.com>
2025-07-04 13:19:08 +0200

    Clarify DB location and expose Flask app

--- config.py
+++ config.py
@@ -6,5 +6,7 @@ load_dotenv(os.path.join(basedir, '.env'))
 
 class Config:
     SECRET_KEY = os.getenv('SECRET_KEY', 'change-me')
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URI', 'sqlite:///intranet.db')
+    SQLALCHEMY_DATABASE_URI = os.getenv(
+        'DATABASE_URI', f"sqlite:///{os.path.join(basedir, 'intranet.db')}"
+    )
     SQLALCHEMY_TRACK_MODIFICATIONS = False
